00e897f72a07cb4e3334c566bbea35a7b7064006
Venom Pratap <venompratap22@gmail.com>
2025-10-16 00:17:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ API_KEY = getenv("API_KEY", None) # youtube song api key, generate free key or b
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/pratap9155/RocksMusic",
+    "https://github.com/fessstygee/RocksMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
@@ -120,6 +120,7 @@ if SUPPORT_GROUP:
         raise SystemExit(
             "[ERROR] - Your SUPPORT_GROUP url is wrong. Please ensure that it starts with https://"
         )
+
 
 
 
